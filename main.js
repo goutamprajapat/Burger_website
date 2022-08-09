21934f847951b06a2cbcb328a76b4699325b8264
goutamprajapat554@gmail.com <goutamprajapat554@gmail.com>
2022-08-09 23:02:57 +0530

    add slider bar

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ const navbar = document.querySelector('.navbar'),
   searchbtn = document.getElementById('search-btn').addEventListener('click', () => {
     search.classList.toggle('active');
   });
+  
+  
+  window.onscroll = () => {
+  navbar.classList.remove('active');
+  search.classList.remove('active');
+};
+
+  
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
